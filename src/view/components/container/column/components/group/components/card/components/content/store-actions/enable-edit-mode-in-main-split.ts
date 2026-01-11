3a63a05a9d0576f977fca800db19cc28d7f7d38a
Dusk <pxiaorong@gmail.com>
2026-01-11 10:49:36 +0800

    Mandala: skip redundant enable edit

--- src/view/components/container/column/components/group/components/card/components/content/store-actions/enable-edit-mode-in-main-split.ts
+++ src/view/components/container/column/components/group/components/card/components/content/store-actions/enable-edit-mode-in-main-split.ts
@@ -4,6 +4,8 @@ export const enableEditModeInMainSplit = (
     view: LineageView,
     nodeId: string,
 ) => {
+    const editing = view.viewStore.getValue().document.editing;
+    if (editing.activeNodeId === nodeId && !editing.isInSidebar) return;
     view.viewStore.dispatch({
         type: 'view/editor/enable-main-editor',
         payload: {
